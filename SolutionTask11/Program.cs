1d9128c34557bc3e809446d03d7acb27c9373da9
Graffchik777 <legione77@mail.ru>
2022-07-29 19:14:59 +0300

    add Task11

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Решение Арта
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+// int numberFierst = number/100;
+// int numberThree = number%10;
+// Console.WriteLine(" Число " + numberFierst + numberThree);
+
+// //Решение Кирилла
+
+int number = new Random().Next(100,1000);
+Console.Write("Число случайное: ");
+Console.WriteLine(number);
+
+int n1 = number/100;
+int n2 = number%10;
+
+Console.Write("Че получилось: ");
+Console.WriteLine(" " + n1 + n2);
