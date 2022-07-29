94012d9bcf60665724c7249366509705f749bc33
Graffchik777 <legione77@mail.ru>
2022-07-29 20:01:56 +0300

    add Task12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputOne = Console.ReadLine();
+string? inputTwo = Console.ReadLine();
+
+if(inputOne != null && inputTwo != null);
+    int inputNunOne = int.Parse(inputOne);
+    int inputNumTwo = int.Parse(inputTwo);
+
+    int diver = inputNunOne%inputNumTwo;
+        if(diver == 0)
+        {
+            Console.WriteLine("Да, делится нацело");
+        }
+            else
+            {
+                Console.WriteLine(diver);
+            }
