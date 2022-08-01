aa7aae866907886441119ab0b72e8b7da6f8b0a2
Graffchik777 <legione77@mail.ru>
2022-08-01 16:51:41 +0300

    add HomeWork15/HomeWork15

---
+++ HomeWork15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Какой день недели по счету: ");
+int WeekDayNumber = Convert.ToInt32(Console.ReadLine());
+
+    if(WeekDayNumber == 6 || WeekDayNumber == 7) 
+  {
+      Console.WriteLine("Че, выходной? - Да!!!");
+  }
+        if(WeekDayNumber < 1 || WeekDayNumber > 7)
+{
+    Console.WriteLine("Я еще в отпуске, или уже в отпуске");
+      }
+    {
+     if(WeekDayNumber >1 || WeekDayNumber <5)   
+     Console.WriteLine("Опять на работу?!((");
+}
