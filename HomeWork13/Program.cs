d43ffdcd0a128f6bd4f00c269b22e5c5632c7d81
Graffchik777 <legione77@mail.ru>
2022-08-01 16:19:08 +0300

    add HomeWork13/Program

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Забьем какое-нибудь число: ");
+int intNumber = Convert.ToInt32(Console.ReadLine());
+string intNumberText = Convert.ToString(intNumber);
+if(intNumberText.Length > 2)
+{
+  Console.WriteLine("Третья цифра: " + intNumberText[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нету...");
+}
