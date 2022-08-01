5d390500d37434589930f8d3f844666c98a79e24
Graffchik777 <legione77@mail.ru>
2022-08-01 16:16:13 +0300

    add HomeWork10/Program "забыли про главную папку"

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Запишем число: ");
+string? namber = Console.ReadLine();
+//int namber = new Random().Next(100,1000);
+//Console.WriteLine(namber);
+string stringNum = namber.ToString();
+Console.Write("От числа осталась серидинка: ");
+Console.Write(stringNum[1]);
+
+
