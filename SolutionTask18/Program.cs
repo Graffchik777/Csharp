4bd8826328c4109f0d56b6e06a2457110c4a6760
Graffchik777 <legione77@mail.ru>
2022-08-04 17:47:54 +0300

    add SolutionTask18/Program

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,19 @@
+﻿void printAnswer(int namber)
+{
+    if(namber == 1) Console.WriteLine(" Допустимо при Х>0 и Y>0");
+    if(namber == 2) Console.WriteLine(" Допустимо при Х<0 и Y>0");
+    if(namber == 3) Console.WriteLine(" Допустимо при Х<0 и Y<0");
+    if(namber == 4) Console.WriteLine(" Допустимо при Х>0 и Y<0");
+
+}
+
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+  
+    //int outNumber = (int)Math.Pow(inputNumber,2);
+    //Console.WriteLine(outNamber);
+    printAnswer(inputNumber);
+}
