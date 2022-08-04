6227f0e28fd96d0d3ecf5a583d3b264d5350e59c
Graffchik777 <legione77@mail.ru>
2022-08-04 21:00:13 +0300

    add HomeWork19/Program

---
+++ HomeWork19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число:     ");
+string? number = Console.ReadLine();
+
+void PolliNum(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Число: {number} это Палиндром.");
+  }
+ else Console.WriteLine($"Число: {number} не ЭТО  не палиТдром");
+}
+if (number!.Length == 5)
+{
+  PolliNum(number);
+}
+
