51c228552c4ca2ad974bf2a82e63cd87502a05cf
Graffchik777 <legione77@mail.ru>
2022-08-04 19:18:22 +0300

    add SolutionTask20/Program

---
+++ SolutionTask20/Program.cs
@@ -0,0 +1,35 @@
+﻿//Заводим координаты точек А и В
+
+int coordXPointA;
+int coordYPointA;
+int coordXPointB;
+int coordYPointB;
+double lengtAB;
+
+void readDataOfPoint()
+{
+    Console.WriteLine("Введите координат Х точки А");
+    coordXPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координат Y точки А");
+    coordYPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координат Х точки B");
+    coordXPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координат Y точки B");
+    coordYPointB = int.Parse(Console.ReadLine());
+}
+
+// Расчет длины
+void colculateLenghAB()
+{
+lengtAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2));
+
+}
+
+
+readDataOfPoint();
+colculateLenghAB();
+
+Console.WriteLine(lengtAB);
