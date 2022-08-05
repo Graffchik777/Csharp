cdfe83d77cf5a9fb0a5cb8710f03e7a2378a073f
Graffchik777 <legione77@mail.ru>
2022-08-05 14:14:30 +0300

    add HomeWork19/Program вопрос по коду

--- HomeWork19/Program.cs
+++ HomeWork19/Program.cs
@@ -14,3 +14,14 @@ if (number!.Length == 5)
   PolliNum(number);
 }
 
+// #include <iostream>
+// int main()
+// {
+//     int ch = -12328;
+//     std::cout<<
+//         ((ch % 10 == ch / 10000 && (ch /10) % 10 == (ch / 1000) % 10) ? 
+//         "it's palindrome" :
+//         "it isn'n palindrome");
+//     return 0;
+// } 
+КАК ЭТОТ КОД ПЕРЕПИСАТЬ В С#
