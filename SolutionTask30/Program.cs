c5507c1b267741bb296569c5745101162d5f72ba
Graffchik777 <legione77@mail.ru>
2022-08-07 21:13:20 +0300

    add SolutionTask30/SolutionTask30

---
+++ SolutionTask30/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+System.Random numberSintezator = new Random();
+
+void VariantNive()
+{
+    int i = 0;
+    Console.Write("[");
+    while(i < 8-1)
+    {
+        Console.Write(numberSintezator.Next(0,2)+",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0,2));
+    Console.Write("]");
+}
+VariantNive();
