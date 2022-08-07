d673291ab1d47838ab9be326c5c40d492ece7b18
Graffchik777 <legione77@mail.ru>
2022-08-07 18:45:48 +0300

    add SolutionTask24/Program

---
+++ SolutionTask24/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+
+long inputNamber = int.Parse(Console.ReadLine());
+long t;
+
+void VariantSimple()
+{
+    long sumOfNumbers = 0;
+    for (long i = 1; i <= inputNamber; i++)
+    {
+        sumOfNumbers += i;
+    }
+    Console.WriteLine("Сумма чисел от 1 до " + inputNamber + " = " + sumOfNumbers);
+
+}
+
+void VariantGauss()
+{
+    long sumOfNumbers = 0;
+    sumOfNumbers = ((1 + inputNamber) * inputNamber) / 2;
+
+    Console.WriteLine("Сумма чисел от 1 до " + inputNamber + " = " + sumOfNumbers);
+
+}
+
+t = Environment.TickCount;
+
+VariantSimple();
+    Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);
+t = Environment.TickCount;
+VariantGauss();
+ Console.WriteLine("Gausse time: {0} ms", Environment.TickCount - t);
