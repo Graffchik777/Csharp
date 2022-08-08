5f7513e7a55e4b38491f308f83cc98b6897d91d1
Graffchik777 <legione77@mail.ru>
2022-08-08 16:57:57 +0300

    add HomeWork29/HomeWork29

---
+++ HomeWork29/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.WriteLine("Настарт! Внимание! Наливааай... ");
+string RandomNumber = Console.ReadLine()??"";
+System.Random numberSintezator = new Random();
+
+void VariantNifigaNeNaiv()
+
+{
+    
+    int i = 0;
+    Console.Write("[");
+    while(i < 9-1)
+    {
+        Console.Write(numberSintezator.Next(5,20)+",");
+        i++;
+    }
+    {
+    Console.Write(numberSintezator.Next(5,20));
+    Console.WriteLine("] ");
+    }
+    Console.WriteLine("                                           ");
+    Console.WriteLine("Получилось!! разделить массив(ответ) и камандную строку)))");
+    Console.WriteLine("                                           ");
+       
+
+
+}
+VariantNifigaNeNaiv();
