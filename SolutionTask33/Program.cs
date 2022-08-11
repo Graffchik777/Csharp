c6d0d903b173c02926112a0498004f74b8ea3c26
Graffchik777 <legione77@mail.ru>
2022-08-11 16:50:41 +0300

    add SolutionTask33/Program

---
+++ SolutionTask33/Program.cs
@@ -0,0 +1,61 @@
+﻿int[] FillingArray()
+{
+    //Выходной массив
+    int[] outArray = new int[12];
+    //Буферная переменная
+    int i = 0;
+    //Создаем экземпляр класса рандомайзер
+    System.Random numberSintezator = new System.Random();
+    while (i < 12)
+    {
+        //Получаем новое значение
+        outArray[i] = numberSintezator.Next(-9, 10);
+        //Увеличиваем инкримент
+        i++;
+    }
+    //Возвращаем значение
+    return outArray;
+}
+
+//Метод печатает массив
+void PrintIntArray(int[] inputArray)
+{
+    //Буферная переменная
+    int i = 0;
+    // Пробегаем все эллементы массива
+    while (i < inputArray.Length - 1)
+    {
+        //Выводим эллемент массива
+        Console.Write(inputArray[i] + "," + " ");
+        //Увеличиваем инкремент
+        i++;
+    }
+    //Выводит эллемент массива
+    Console.WriteLine(inputArray[i]);
+}
+
+
+//Метод решения задачи 33 наивный
+bool ColculateTask(int[] inputArray, int searchNumber)
+{
+    bool resultSearch = false;
+    int i = 0;
+    while (i < 12)
+    {
+        if (inputArray[i] == searchNumber)
+        {
+            resultSearch = true;
+            break;
+        }
+
+        i++;
+    }
+    return resultSearch;
+}
+
+int[] buferArray = FillingArray();
+PrintIntArray(buferArray);
+Console.WriteLine("Введите число для поиска:  ");
+int searchNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine(ColculateTask(buferArray, searchNumber));
