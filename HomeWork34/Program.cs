b85ae0f434a6e8990197068e32b720bbb658a2a2
Graffchik777 <legione77@mail.ru>
2022-08-12 14:24:20 +0300

    add HomeWork34/HomeWork34

---
+++ HomeWork34/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+int[] digitals = new int[19];
+Console.WriteLine("Массивчик собрали: ");
+Console.WriteLine(" ");
+
+void FillingArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int PositiveNumber(int[] array)
+{
+    int quanity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            quanity++;
+        }
+    }
+    return quanity;
+}
+
+FillingArray(digitals, 100, 1000);
+WriteArray(digitals);
+Console.WriteLine();
+
+int quanity = PositiveNumber(digitals);
+Console.WriteLine($"Чётных чисел тута: {quanity}");
+Console.WriteLine(" ");
+
