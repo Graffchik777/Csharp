e4669f0bf1c8fe32be9208fe65da25415bf24070
Graffchik777 <legione77@mail.ru>
2022-08-12 15:39:18 +0300

    add HomeWork36/HomeWork36

---
+++ HomeWork36/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+Console.WriteLine(" ");
+Console.WriteLine("Скока челенов в этом массиве?");
+
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+FillingArray(numbers);
+
+Console.WriteLine("Во чё вышло: ");
+
+PrintInArray(numbers);
+int summa = 0;
+
+for (int i = 0; i < numbers.Length; i += 2)
+    summa = summa + numbers[i];
+
+Console.WriteLine($"всего {numbers.Length} чисел");
+Console.WriteLine($"сумма всех челенов на нечётных местах = {summa}");
+Console.WriteLine(" ");
+
+
+int[] FillingArray(int[] numbers)
+{
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        numbers[j] = new Random().Next(-10, 10);
+    }
+    return numbers;
+}
+
+int[] PrintInArray(int[] numbers)
+{
+    int j = 0;
+    Console.WriteLine(" ");
+
+    while (j < numbers.Length - 1)
+    {
+        Console.Write(numbers[j] + "," + " ");
+        j++;
+    }
+
+    Console.WriteLine(numbers[j]);
+    Console.WriteLine(" ");
+    return numbers;
+}
+
