d367cfca561687cd04f7cf336f963c2c9c51fa85
Graffchik777 <legione77@mail.ru>
2022-08-15 16:41:12 +0300

    add HomeWork41/HomeWork41

---
+++ HomeWork41/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+Console.Write($"Нажмите сколько хотите циферок: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int[] arrayDigit = new int[i];
+
+void InputDigits(int j)
+
+{
+    j = 0;
+    while (j < i)
+    {
+        Console.Write($"Нажмите {j + 1} циферку: ");
+        arrayDigit[j] = Convert.ToInt32(Console.ReadLine());
+        j++;
+    }
+
+}
+
+int Compare(int[] arrayDigit)
+{
+    int count = 0;
+    for (int j = 0; j < arrayDigit.Length; j++)
+    {
+        if (arrayDigit[j] > 0)
+            count += 1;
+    }
+    return count;
+}
+
+InputDigits(i);
+Console.WriteLine(" ");
+Console.WriteLine($"ПоложительнАх циферок: {Compare(arrayDigit)} ");
+Console.WriteLine(" ");
