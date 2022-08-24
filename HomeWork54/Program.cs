632c582b3226b53dbe8a9930bcf680d820aa834a
Graffchik777 <legione77@mail.ru>
2022-08-24 18:02:36 +0300

    add HomeWork54/HomeWork54

---
+++ HomeWork54/Program.cs
@@ -0,0 +1,63 @@
+﻿//Задача 54: Задайте двумерный массив. 
+//Напишите программу, которая упорядочит по убыванию 
+//элементы каждой строки двумерного массива.
+//------------------------------------------------------------------------
+
+Console.WriteLine("Скока строк?");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Скока столбиков?");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] digits = new int[lines, columns];
+ArrayRandDigits(digits);
+Console.WriteLine();
+Console.WriteLine("Че было: ");
+PrintArray(digits);
+
+for (int i = 0; i < digits.GetLength(0); i++)
+{
+    for (int j = 0; j < digits.GetLength(1) - 1; j++)
+    {
+        for (int sort = 0; sort < digits.GetLength(1) - 1; sort++)
+        {
+            if (digits[i, sort] < digits[i, sort + 1]) 
+            {
+                int temp = 0;
+                temp = digits[i, sort];
+                digits[i, sort] = digits[i, sort + 1];
+                digits[i, sort + 1] = temp;
+            }
+        }
+    }
+}
+
+
+
+Console.WriteLine();
+Console.WriteLine("Че стало: ");
+
+PrintArray(digits);
+
+void ArrayRandDigits(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-101, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.Write("]");
+        Console.WriteLine(" ");
+    }
+}
