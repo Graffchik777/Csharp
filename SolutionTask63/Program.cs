0798a4c91d058d6eef397019ceac2e57cc4a43bf
Graffchik777 <legione77@mail.ru>
2022-08-25 15:23:16 +0300

    add SolutionTask63/Program

---
+++ SolutionTask63/Program.cs
@@ -0,0 +1,52 @@
+﻿// Рекурсия
+
+
+// РЕПКА(200Н силы): Дед(100Н силы)-Бабка(50Н силы)-Внучка(25)
+//                   -Жучка(12Н )-Машка(12Н)-Мышка(1Н)Stop
+
+// void Power(int askedPower)
+// {
+//     if(askedPower>out1)
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         Power(askedPower);
+//     }
+// }
+
+
+// Задача 63: Задайте значание N. Напишите программу которая 
+// выведет все натуральные числа в промежутке от 1 до N.
+//---------------------------------------------------------
+Console.Clear();
+
+int ReadData()
+{
+    Console.WriteLine("Задайте число: ");
+    return int.Parse(Console.ReadLine());
+}
+
+//Стек
+//1 4 - 3 out
+//2 3 - 2 out 2
+//3 2 -   out 1
+
+
+int NuturalNumberPrinter(int num)
+{
+    if(num == 2 )return 1;
+    else
+    {
+        num--;
+        Console.WriteLine(num);
+        Console.Write(NuturalNumberPrinter(num) + " ");
+    }
+    return num;
+}
+
+
+int inputNumber = ReadData();
+NuturalNumberPrinter(inputNumber+2);
+
